8a20718b14d84dcbb79b4c68d5d0157749ad8def
Sarang Sami <sami@novin.dev>
2022-05-06 17:38:50 +0430

    feat: :art: Gatsby and Typescript with MUI5

---
+++ gatsby-config.ts
@@ -0,0 +1,36 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `gatsby-ts-mui`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    'gatsby-plugin-top-layout',
+    'gatsby-plugin-react-helmet',
+    // If you want to use styled components you should add the plugin here.
+    // 'gatsby-plugin-styled-components',
+    'gatsby-plugin-mui-emotion',
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
+
+export default config;
